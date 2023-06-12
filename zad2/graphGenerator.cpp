6105bd87e4637e0d480ebc7cda45c44b21192732
Deri <dominikcybulski@protonmail.com>
2023-06-13 00:39:15 +0200

    Implements generation of matrix representation of a graph

---
+++ zad2/graphGenerator.cpp
@@ -0,0 +1,95 @@
+#include "graphGenerator.h"
+MatrixN * generateMatrix(int size, int density, bool twoWays, bool negativeValues){
+    if(density >= 100 || density < 0)
+        throw "Wrong density!";
+    MatrixN * toReturn = new MatrixN(size);
+
+   
+    //Creates minimal spanning tree
+    for(int i = 0; i < size-1; i++){
+        int weight = generateEdgeWeight(negativeValues);
+        toReturn -> insert(i, i+1, weight);
+        if(twoWays)
+            toReturn -> insert(i+1, i, weight);
+    }
+
+    //Calculates density
+    if(density == 99){
+        int maxEdges = (size * (size - 1));
+        if(twoWays) {
+            maxEdges /= 2;
+        }
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(i == j)
+                    continue;
+                if(toReturn -> getEdgeWage(i, j) != 0)
+                    continue;
+                int weight = generateEdgeWeight(negativeValues);
+                toReturn -> insert(i, j, weight);
+                if(twoWays)
+                    toReturn -> insert(j, i, weight);
+            }
+        }
+        int edgesToRemove = maxEdges / 100 + 1;
+        printf("Max edges: %d\n", maxEdges);
+        printf("Edges to remove: %d\n", edgesToRemove);
+        while(edgesToRemove > 0){
+            int startNode = rand()%size;
+            int endNode = rand()%size;
+            if(startNode == endNode)
+                continue;
+            if(toReturn -> getEdgeWage(startNode, endNode) == 0)
+                continue;
+            printf("Removing %d -> %d", startNode, endNode);
+            toReturn -> insert(startNode, endNode, 0);
+            if(twoWays)
+                toReturn -> insert(endNode, startNode, 0);
+            edgesToRemove--;
+        }
+    }
+    else{
+        int maxEdges = (size * (size - 1));
+        if(twoWays) {
+            maxEdges /= 2;
+        }
+        //printf("Max edges: %d\n", maxEdges);
+        int lackingEdges = ((maxEdges * density) / 100) - size + 1;
+    
+        //printf("Lacking edges: %d\n", lackingEdges);
+        while(lackingEdges > 0){
+            int startNode = rand()%size;
+            int endNode = rand()%size;
+            if(startNode == endNode)
+                continue;
+            if(toReturn -> getEdgeWage(startNode, endNode) != 0){
+                continue;
+            }
+            int weight = generateEdgeWeight(negativeValues);
+            //printf("Picked %d -> %d with value: %d\n", startNode, endNode, weight);
+            toReturn -> insert(startNode, endNode, weight);
+            if(twoWays){
+                toReturn -> insert(endNode, startNode, weight);
+            }
+            lackingEdges--;
+        }
+    }
+    return toReturn;
+
+};
+ListN * generateList(int size, int density, bool twoWays, bool negativeValues){
+    return nullptr;
+};
+
+int generateEdgeWeight(bool negativeValues){
+    int toReturn = 0;
+    int randomRange = 9;
+    int randomMod = 1;
+    if(negativeValues)
+        randomMod -= 5;
+    toReturn = rand()%randomRange + randomMod;
+    if(toReturn == 0) 
+        return generateEdgeWeight(negativeValues);
+    return toReturn;
+}
+
---
+++ zad2/graphGenerator.h
@@ -0,0 +1,6 @@
+#include "matrixN.cpp"
+#include "listN.cpp"
+
+MatrixN * generateMatrix(int size, int density, bool twoWays=true, bool negativeValues=false);
+ListN * generateList(int size, int density, bool twoWays=true, bool negativeValues=false);
+int generateEdgeWeight(bool negativeValues=false);
