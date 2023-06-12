6652d485f32870e9bb5d3d9b5238537c2c09ba93
Deri <dominikcybulski@protonmail.com>
2023-06-13 00:46:59 +0200

    Implements random geeneration of list representations of a graph

--- zad2/graphGenerator.h
+++ zad2/graphGenerator.h
@@ -1,6 +1,9 @@
+#ifndef GENERATE
+#define GENERATE
 #include "matrixN.cpp"
 #include "listN.cpp"
 
 MatrixN * generateMatrix(int size, int density, bool twoWays=true, bool negativeValues=false);
 ListN * generateList(int size, int density, bool twoWays=true, bool negativeValues=false);
 int generateEdgeWeight(bool negativeValues=false);
+#endif
